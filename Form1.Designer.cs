c53c2633a0f7d0d7d65f8f57cee32ad562b9ea16
FreshWaterGames <70551495+FreshWaterGames@users.noreply.github.com>
2024-06-27 09:18:32 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WallpaperChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.load_button = new System.Windows.Forms.Button();
+            this.save_button = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.UI_Changer = new System.Windows.Forms.Button();
+            this.LoadFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(512, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(494, 360);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Load or Create Photo Libary.\r\n";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(512, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(285, 320);
+            this.panel1.TabIndex = 5;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(548, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 40);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Drag 24 photo\'s into the blue\r\nsection to create a photo libary.\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // load_button
+            // 
+            this.load_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.load_button.Location = new System.Drawing.Point(242, 19);
+            this.load_button.Name = "load_button";
+            this.load_button.Size = new System.Drawing.Size(115, 35);
+            this.load_button.TabIndex = 8;
+            this.load_button.Text = "Load Photos";
+            this.load_button.UseVisualStyleBackColor = true;
+            this.load_button.Click += new System.EventHandler(this.load_button_Click);
+            // 
+            // save_button
+            // 
+            this.save_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save_button.Location = new System.Drawing.Point(391, 19);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(115, 35);
+            this.save_button.TabIndex = 9;
+            this.save_button.Text = "Save Photos";
+            this.save_button.UseVisualStyleBackColor = true;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StopButton.Location = new System.Drawing.Point(722, 404);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 34);
+            this.StopButton.TabIndex = 10;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.CreatePrompt = true;
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // UI_Changer
+            // 
+            this.UI_Changer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_Changer.Location = new System.Drawing.Point(607, 404);
+            this.UI_Changer.Name = "UI_Changer";
+            this.UI_Changer.Size = new System.Drawing.Size(99, 34);
+            this.UI_Changer.TabIndex = 11;
+            this.UI_Changer.Text = "Uncapped";
+            this.UI_Changer.UseVisualStyleBackColor = true;
+            this.UI_Changer.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // LoadFolder
+            // 
+            this.LoadFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadFolder.Location = new System.Drawing.Point(242, 19);
+            this.LoadFolder.Name = "LoadFolder";
+            this.LoadFolder.Size = new System.Drawing.Size(115, 37);
+            this.LoadFolder.TabIndex = 12;
+            this.LoadFolder.Text = "Load Folder";
+            this.LoadFolder.UseVisualStyleBackColor = true;
+            this.LoadFolder.Visible = false;
+            this.LoadFolder.Click += new System.EventHandler(this.LoadFolder_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.radioButton1.Location = new System.Drawing.Point(387, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(96, 24);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "5 Mintues";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.radioButton2.Location = new System.Drawing.Point(387, 27);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(105, 24);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "30 Minutes";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.radioButton3.Location = new System.Drawing.Point(387, 48);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(75, 24);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "1 Hour";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.LoadFolder);
+            this.Controls.Add(this.UI_Changer);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.load_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button load_button;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button UI_Changer;
+        private System.Windows.Forms.Button LoadFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace WallpaperChanger
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+
+        UInt32 SPI_SETWALL = 0x14;
+        UInt32 SPIF_UPDATE = 0x01;
+        UInt32 SPIF_SWEDINI = 0x2;
+
+        // creates time object
+        DateTime now = DateTime.Now;
+        Random random = new Random();
+        string hours = "0";
+        String getFile;
+        string folderpath;
+        string completepath;
+        string choice;
+        int imageCount = 0;
+        int intervals = 0;
+        bool photosloaded = false;
+        bool folderloaded = false;
+        bool unlocked = true;
+
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // setup and runs application
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (photosloaded == true)
+            {
+                hours = now.Hour.ToString();
+                textBox1.Text = hours;
+                // Sets wallpaper
+                SystemParametersInfo(SPI_SETWALL, 0, dict[hours], SPIF_UPDATE | SPIF_SWEDINI);
+                timer1.Enabled = true;
+            }
+
+            else if (folderloaded == true)
+            {
+                if (radioButton1.Checked == true)
+                {
+                    // 5 mins
+                    intervals = 300000;
+                }
+                if (radioButton2.Checked == true)
+                {
+                    // 30 mins
+                    intervals = 1800000;
+                }
+                if (radioButton3.Checked == true)
+                {
+                    // 1 hour
+                    intervals = 3600000;
+                }
+
+                timer2.Interval = intervals;
+                // get directory
+                var filepath = Directory.GetFiles(completepath);
+                // gets random file from directory
+                choice = filepath[random.Next(filepath.Length)].ToString();
+                pictureBox1.Image = Image.FromFile(choice);
+                // Sets wallpaper
+                SystemParametersInfo(SPI_SETWALL, 0, choice, SPIF_UPDATE | SPIF_SWEDINI);
+                timer2.Enabled = true;
+            }
+            else
+            {
+                textBox1.Text = "Please Load Photo Libary.";
+            }
+        }
+
+        // 24hr timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            now = DateTime.Now;
+            hours = now.Hour.ToString();
+            textBox1.Text = "Running...";
+            // Sets wallpaper
+            SystemParametersInfo(SPI_SETWALL, 0, dict[hours], SPIF_UPDATE | SPIF_SWEDINI);
+        }
+
+        // uncapped timer
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            var filepath = Directory.GetFiles(completepath);
+            choice = filepath[random.Next(filepath.Length)].ToString();
+            textBox1.Text = "Running...";
+            // Sets wallpaper
+            SystemParametersInfo(SPI_SETWALL, 0, choice, SPIF_UPDATE | SPIF_SWEDINI);
+        }
+
+
+        // stores image into dictionary
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] filelist = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string done = filelist[0];
+            pictureBox1.Image = Image.FromFile(done);
+            if (imageCount <= 24)
+            {
+                textBox1.Text = imageCount.ToString();
+                dict.Add(imageCount.ToString(), done);
+                imageCount++;
+            }
+            else
+            {
+                textBox1.Text = "Photos Ready!";
+                photosloaded = true;
+
+            }
+        }
+
+        // gets path from draged file
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect= DragDropEffects.None;
+            }
+        }
+
+        // saves photo
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            // searalization is the process of converting info into bytes to save / transmit them
+            File.WriteAllText(saveFileDialog1.FileName + ".txt", new JavaScriptSerializer().Serialize(dict));
+            textBox1.Text = "Photo Libary Saved!!";
+            
+        }
+
+        // loads photo libary
+        private void load_button_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                getFile = openFileDialog1.FileName;
+                // deserialization rebuilds the information back to its original state
+                var dict_load = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(File.ReadAllText(getFile));
+                textBox1.Text = "Photo Libary Loaded!!";
+                dict = dict_load;
+                pictureBox1.Image = Image.FromFile(dict["0"]);
+                photosloaded = true;
+                folderloaded = false;
+            }
+        }
+
+        // loads photo folder
+        private void LoadFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderpath = folderBrowserDialog1.SelectedPath;
+                completepath = folderpath;
+                folderloaded = true;
+                photosloaded = false;
+                textBox1.Text = "Folder Loaded!";
+            }
+        }
+
+        // switches between 24hr and dynamic wallpaper modes
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (unlocked == true)
+            {
+                UI_Changer.Text = "24hr";
+                textBox1.Text = "Load Photo Folder.";
+                LoadFolder.Visible = true;
+                radioButton1.Visible = true;
+                radioButton2.Visible = true;
+                radioButton3.Visible = true;
+                load_button.Visible = false;
+                save_button.Visible = false;
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+            }
+            if (unlocked == false)
+            {
+                UI_Changer.Text = "Uncapped";
+                textBox1.Text = "Load or Create Photo Libary.\r\n";
+                LoadFolder.Visible = false;
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                radioButton3.Visible = false;
+                load_button.Visible = true;
+                save_button.Visible = true;
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+            }
+            unlocked = !unlocked;
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
